8df8a548258840fed31b9ae9e1135fd57faf41b0
LayaCube <layapeltier19@gmail.com>
2023-11-25 14:26:49 +0100

    Added check for save file existence, corrected save. Closed #5

--- src/snake.h
+++ src/snake.h
@@ -17,6 +17,20 @@
 #include <vector>
 #include "constants.hpp"
 #include <bits/stdc++.h> 
+#ifndef __has_include
+  static_assert(false, "__has_include not supported");
+#else
+#  if __cplusplus >= 201703L && __has_include(<filesystem>)
+#    include <filesystem>
+     namespace fs = std::filesystem;
+#  elif __has_include(<experimental/filesystem>)
+#    include <experimental/filesystem>
+     namespace fs = std::experimental::filesystem;
+#  elif __has_include(<boost/filesystem.hpp>)
+#    include <boost/filesystem.hpp>
+     namespace fs = boost::filesystem;
+#  endif
+#endif
 using namespace std;
 
 
